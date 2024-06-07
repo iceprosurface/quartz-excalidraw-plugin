a2b5ce5f362d064b9ef5d9fa5fb4a51f54a552f4
icepro <icepro@xd.com>
2024-06-07 17:52:04 +0800

    fix: viewport not fit

--- src/lib.tsx
+++ src/lib.tsx
@@ -1,20 +1,40 @@
 import { Excalidraw } from "@excalidraw/excalidraw";
-import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
 import { render } from 'preact';
 import LZString from 'lz-string';
+import { ExcalidrawImperativeAPI, ExcalidrawInitialDataState } from "@excalidraw/excalidraw/types/types";
+import { useEffect, useState } from "preact/hooks";
 type ExcalidrawProps = {
-  width?: string,
-  height?: string
+  width?: number,
+  height?: number
 }
 function App(props: {
-  element: readonly ExcalidrawElement[] | null,
-
+  data?: ExcalidrawInitialDataState
 } & ExcalidrawProps) {
-  return <div style={{ width: props.width, height: props.height }}>
+  const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI | null>(null);
+  useEffect(() => {
+    (async () => {
+      if (excalidrawAPI) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        excalidrawAPI.scrollToContent(excalidrawAPI.getSceneElements(), {
+          fitToViewport: true,
+          animate: true,
+          duration: 300,
+        });
+        console.log('excalidrawAPI');
+      }
+    })()
+  }, [excalidrawAPI])
+  return <div style={{ width: '100%', height: '100%' }}>
     <Excalidraw
+      excalidrawAPI={(api) => setExcalidrawAPI(api)}
       initialData={{
-        elements: props.element,
-        scrollToContent: true
+        ...props.data,
+        appState: {
+          ...props.data?.appState,
+          width: props.width,
+          height: props.height,
+          zenModeEnabled: true
+        },
       }}
       viewModeEnabled={true}
     >
@@ -22,22 +42,21 @@ function App(props: {
   </div>
 }
 
-export function mountApp(element: HTMLElement, initialData: readonly ExcalidrawElement[] | null, options: ExcalidrawProps) {
-  console.log(initialData)
-  render(<App element={initialData} {...options} />, element);
+export function mountApp(element: HTMLElement, initialData: ExcalidrawInitialDataState, options: ExcalidrawProps) {
+  render(<App data={initialData} {...options} />, element);
 }
 export const decompress = (data: string,): string => {
   return LZString.decompressFromBase64(data.replaceAll("\n", "").replaceAll("\r", ""));
 };
 
-export function decodeData(data: string): ExcalidrawElement[] {
+export function decodeData(data: string): ExcalidrawInitialDataState {
   const parts = data.split("\n# Drawing\n```compressed-json\n");
-  if (parts.length !== 2) return []
+  if (parts.length !== 2) return {}
   const compressed = parts[1].split("\n```\n%%");
-  if (compressed.length !== 2) return []
+  if (compressed.length !== 2) return {}
   const decompressed = decompress(compressed[0]);
   if (!decompressed) {
-    return []
+    return {}
   }
-  return JSON.parse(decompressed)?.elements ?? [];
+  return JSON.parse(decompressed) ?? [];
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import data from "./test.excalidraw.md?raw";
 import { decodeData, mountApp } from "./lib";
 
 mountApp(document.getElementById("app")!, decodeData(data), {
-  width: "100%",
-  height: "100%"
+  width: 400,
+  height: 500
 });
